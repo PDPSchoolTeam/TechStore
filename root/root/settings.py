09e90ba96ac9b5c9a0c46a8c4dff95e92259066e
Saidxoja's page <fattoxovjavoxir@gmail.com>
2024-11-16 00:01:20 +0500

    requirements

--- root/root/settings.py
+++ root/root/settings.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 import os
+import dj_database_url
+
+# Database
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
